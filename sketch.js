a5cdebb67b6943190e0f8356cb7e619b0cf227b7
MarkAllman64 <markallman51@gmail.com>
2018-02-10 16:34:55 -0600

    Crazy Stuff

--- sketch.js
+++ sketch.js
@@ -1,10 +1,42 @@
+var xLocation = 100;
+var yLocation = 100;
+var xSpeed = 0;
+var ySpeed = 0;
+
+
 function setup() {
   createCanvas(600, 600);
 }
 
+function keyPressed() {
+  if (keyCode == DOWN_ARROW) {
+    xSpeed = xSpeed + 1;
+  }
+  if (keyCode == UP_ARROW) {
+    xSpeed = xSpeed - 1;
+  }
+  if (keyCode == LEFT_ARROW) {
+    ySpeed = ySpeed + 1
+  }
+  if (keyCode == RIGHT_ARROW) {
+    ySpeed = ySpeed - 1
+  }
+}
+
 function draw() {
-  var c = color('magenta');
-fill(c); // Use 'c' as fill color
-  ellipse(100, 100, 50, 50);
-  rect(mouseX, mouseY, 60, 60);
+  background(221, 28, 119);
+  stroke('blue');
+  strokeWeight(20);
+   var c = color('magenta');
+   fill(c);
+  ellipse(xLocation, yLocation, 50, 50);
+   rect(xLocation - 30, xLocation + 30, 60, 60);
+
+xLocation = xLocation + xSpeed
+yLocation = yLocation + ySpeed
+
+textSize (18);
+strokeWeight(2);
+text(xSpeed, 100, 20);
+text(ySpeed, 150, 20);
 }
